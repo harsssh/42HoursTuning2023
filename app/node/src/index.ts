a03f8694854af00e9f7b6d3e0d78c4d428f51554
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-17 03:54:03 +0900

    newrelic

--- app/node/src/index.ts
+++ app/node/src/index.ts
@@ -1,5 +1,10 @@
-require('newrelic')
+import newrelic from 'newrelic';
 import { app } from "./app";
+
+newrelic.instrumentLoadedModule(
+    'express',
+    app
+);
 
 const port = 8000;
 app.listen(port, () => {
