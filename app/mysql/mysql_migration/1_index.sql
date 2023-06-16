f4365cd32cb68733a45df1ab08a3615aff4f2e7c
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-17 08:51:26 +0900

    fix drop index

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -1,5 +1,5 @@
 CALL DropIndexIfExists('user', 'idx_mail_password');
 CREATE INDEX idx_mail_password ON user (`mail`, `password`);
 
-CALL DropIndexIfExists('user', 'idx_user_id');
+CALL DropIndexIfExists('user', 'idx_linked_user_id');
 CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
