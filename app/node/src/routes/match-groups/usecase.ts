8619c35f284ef26e5f0303ea814c8162df9c9909
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-18 01:08:20 +0900

    restore match group usecase

--- app/node/src/routes/match-groups/usecase.ts
+++ app/node/src/routes/match-groups/usecase.ts
@@ -1,7 +1,3 @@
-import { RowDataPacket } from "mysql2/promise"; //
-import pool from "../../util/mysql"; //
-import { convertToUserForFilter } from "../../model/utils"; //
-
 import { v4 as uuidv4 } from "uuid";
 import {
   MatchGroupDetail,
@@ -10,7 +6,7 @@ import {
 } from "../../model/types";
 import {
   getMatchGroupDetailByMatchGroupId,
-  // getUserIdsBeforeMatched,
+  getUserIdsBeforeMatched,
   hasSkillNameRecord,
   insertMatchGroup,
 } from "./repository";
@@ -28,89 +24,60 @@ export const checkSkillsRegistered = async (
   return;
 };
 
-export const getAllCandidates = async (
-  matchGroupConfig: MatchGroupConfig,
-  ownerDepartmentName: string,
-  ownerOfficeName: string
-): Promise<UserForFilter[]> => {
-  const { departmentFilter, officeFilter, skillFilter } = matchGroupConfig;
-
-  // Building the base of the SQL query
-  let sqlQuery = `SELECT * FROM user 
-  JOIN skill_member ON user.user_id = skill_member.user_id
-  JOIN skill ON skill_member.skill_id = skill.skill_id
-  JOIN department_role_member ON department_role_member.user_id = user.user_id
-  JOIN department ON department.department_id = department_role_member.department_id`;
-
-  // An array to hold the parameters of the SQL query
-  const sqlParams = [];
-
-  // 範囲を制限したい
-  // Adding the filters to the SQL query
-  if (
-    departmentFilter !== "none" ||
-    officeFilter !== "none" ||
-    skillFilter.length > 0
-  ) {
-    sqlQuery += " WHERE ";
-    const conditions = [];
-
-    if (departmentFilter !== "none") {
-      conditions.push("department_name = ?");
-      sqlParams.push(ownerDepartmentName);
-    }
-
-    if (officeFilter !== "none") {
-      conditions.push("office_name = ?");
-      sqlParams.push(ownerOfficeName);
-    }
-
-    if (skillFilter.length > 0) {
-      // This assumes that the skillFilter is an array of skill IDs
-      conditions.push(`skill_name IN (?)`);
-      sqlParams.push(skillFilter);
-    }
-
-    sqlQuery += conditions.join(" AND ");
-  }
-
-  const [userRows] = await pool.query<RowDataPacket[]>(sqlQuery, sqlParams);
-
-  // We assume that there's a function convertToUserForFilter which can convert the raw SQL result to the UserForFilter type
-  return userRows.map(convertToUserForFilter);
-};
-
 export const createMatchGroup = async (
   matchGroupConfig: MatchGroupConfig,
   timeout?: number
 ): Promise<MatchGroupDetail | undefined> => {
   const owner = await getUserForFilter(matchGroupConfig.ownerId);
-  const members: UserForFilter[] = [owner];
+  let members: UserForFilter[] = [owner];
   const startTime = Date.now();
-
-  // Get all candidates who pass the filter
-  const candidates = await getAllCandidates(matchGroupConfig, owner.departmentName, owner.officeName);
-  
-  if (members.length < matchGroupConfig.numOfMembers) {
-    return undefined; 
-  }
-
-  // while (members.length < matchGroupConfig.groupCount) {
   while (members.length < matchGroupConfig.numOfMembers) {
-    // Check for timeout
-    if (timeout && Date.now() - startTime > timeout) {
-      return undefined;
+    // デフォルトは50秒でタイムアウト
+    if (Date.now() - startTime > (!timeout ? 50000 : timeout)) {
+      console.error("not all members found before timeout");
+      return;
     }
-
-    // Get a random candidate
-    const candidateIndex = Math.floor(Math.random() * candidates.length);
-    const candidate = candidates[candidateIndex];
-
-    // Add candidate to group members
-    members.push(candidate);
-
-    // Remove candidate from the candidates list
-    candidates.splice(candidateIndex, 1);
+    const candidate = await getUserForFilter();
+    if (
+      matchGroupConfig.departmentFilter !== "none" &&
+      !isPassedDepartmentFilter(
+        matchGroupConfig.departmentFilter,
+        owner.departmentName,
+        candidate.departmentName
+      )
+    ) {
+      console.log(`${candidate.userId} is not passed department filter`);
+      continue;
+    } else if (
+      matchGroupConfig.officeFilter !== "none" &&
+      !isPassedOfficeFilter(
+        matchGroupConfig.officeFilter,
+        owner.officeName,
+        candidate.officeName
+      )
+    ) {
+      console.log(`${candidate.userId} is not passed office filter`);
+      continue;
+    } else if (
+      matchGroupConfig.skillFilter.length > 0 &&
+      !matchGroupConfig.skillFilter.some((skill) =>
+        candidate.skillNames.includes(skill)
+      )
+    ) {
+      console.log(`${candidate.userId} is not passed skill filter`);
+      continue;
+    } else if (
+      matchGroupConfig.neverMatchedFilter &&
+      !(await isPassedMatchFilter(matchGroupConfig.ownerId, candidate.userId))
+    ) {
+      console.log(`${candidate.userId} is not passed never matched filter`);
+      continue;
+    } else if (members.some((member) => member.userId === candidate.userId)) {
+      console.log(`${candidate.userId} is already added to members`);
+      continue;
+    }
+    members = members.concat(candidate);
+    console.log(`${candidate.userId} is added to members`);
   }
 
   const matchGroupId = uuidv4();
@@ -127,99 +94,29 @@ export const createMatchGroup = async (
   return await getMatchGroupDetailByMatchGroupId(matchGroupId);
 };
 
-// export const createMatchGroup = async (
-//   matchGroupConfig: MatchGroupConfig,
-//   timeout?: number
-// ): Promise<MatchGroupDetail | undefined> => {
-//   const owner = await getUserForFilter(matchGroupConfig.ownerId);
-//   let members: UserForFilter[] = [owner];
-//   const startTime = Date.now();
-//   while (members.length < matchGroupConfig.numOfMembers) {
-//     // デフォルトは50秒でタイムアウト
-//     if (Date.now() - startTime > (!timeout ? 50000 : timeout)) {
-//       console.error("not all members found before timeout");
-//       return;
-//     }
-//     const candidate = await getUserForFilter();
-//     if (
-//       matchGroupConfig.departmentFilter !== "none" &&
-//       !isPassedDepartmentFilter(
-//         matchGroupConfig.departmentFilter,
-//         owner.departmentName,
-//         candidate.departmentName
-//       )
-//     ) {
-//       console.log(`${candidate.userId} is not passed department filter`);
-//       continue;
-//     } else if (
-//       matchGroupConfig.officeFilter !== "none" &&
-//       !isPassedOfficeFilter(
-//         matchGroupConfig.officeFilter,
-//         owner.officeName,
-//         candidate.officeName
-//       )
-//     ) {
-//       console.log(`${candidate.userId} is not passed office filter`);
-//       continue;
-//     } else if (
-//       matchGroupConfig.skillFilter.length > 0 &&
-//       !matchGroupConfig.skillFilter.some((skill) =>
-//         candidate.skillNames.includes(skill)
-//       )
-//     ) {
-//       console.log(`${candidate.userId} is not passed skill filter`);
-//       continue;
-//     } else if (
-//       matchGroupConfig.neverMatchedFilter &&
-//       !(await isPassedMatchFilter(matchGroupConfig.ownerId, candidate.userId))
-//     ) {
-//       console.log(`${candidate.userId} is not passed never matched filter`);
-//       continue;
-//     } else if (members.some((member) => member.userId === candidate.userId)) {
-//       console.log(`${candidate.userId} is already added to members`);
-//       continue;
-//     }
-//     members = members.concat(candidate);
-//     console.log(`${candidate.userId} is added to members`);
-//   }
+const isPassedDepartmentFilter = (
+  departmentFilter: string,
+  ownerDepartment: string,
+  candidateDepartment: string
+) => {
+  return departmentFilter === "onlyMyDepartment"
+    ? ownerDepartment === candidateDepartment
+    : ownerDepartment !== candidateDepartment;
+};
 
-//   const matchGroupId = uuidv4();
-//   await insertMatchGroup({
-//     matchGroupId,
-//     matchGroupName: matchGroupConfig.matchGroupName,
-//     description: matchGroupConfig.description,
-//     members,
-//     status: "open",
-//     createdBy: matchGroupConfig.ownerId,
-//     createdAt: new Date(),
-//   });
+const isPassedOfficeFilter = (
+  officeFilter: string,
+  ownerOffice: string,
+  candidateOffice: string
+) => {
+  return officeFilter === "onlyMyOffice"
+    ? ownerOffice === candidateOffice
+    : ownerOffice !== candidateOffice;
+};
 
-//   return await getMatchGroupDetailByMatchGroupId(matchGroupId);
-// };
-
-// const isPassedDepartmentFilter = (
-//   departmentFilter: string,
-//   ownerDepartment: string,
-//   candidateDepartment: string
-// ) => {
-//   return departmentFilter === "onlyMyDepartment"
-//     ? ownerDepartment === candidateDepartment
-//     : ownerDepartment !== candidateDepartment;
-// };
-
-// const isPassedOfficeFilter = (
-//   officeFilter: string,
-//   ownerOffice: string,
-//   candidateOffice: string
-// ) => {
-//   return officeFilter === "onlyMyOffice"
-//     ? ownerOffice === candidateOffice
-//     : ownerOffice !== candidateOffice;
-// };
-
-// const isPassedMatchFilter = async (ownerId: string, candidateId: string) => {
-//   const userIdsBeforeMatched = await getUserIdsBeforeMatched(ownerId);
-//   return userIdsBeforeMatched.every(
-//     (userIdBeforeMatched) => userIdBeforeMatched !== candidateId
-//   );
-// };
+const isPassedMatchFilter = async (ownerId: string, candidateId: string) => {
+  const userIdsBeforeMatched = await getUserIdsBeforeMatched(ownerId);
+  return userIdsBeforeMatched.every(
+    (userIdBeforeMatched) => userIdBeforeMatched !== candidateId
+  );
+};
