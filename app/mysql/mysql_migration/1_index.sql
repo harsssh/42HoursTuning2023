d02c8a0e423c46dfd1e0be1ce99b37644442b678
Yuki Ichinose <yichinos@c1r24s11.42tokyo.jp>
2023-06-17 09:43:16 +0900

    add FULLTEXT

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -3,3 +3,13 @@ CREATE INDEX idx_mail_password ON user (`mail`, `password`);
 
 CALL DropIndexIfExists('user', 'idx_office_id');
 CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
+
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_user_name (user_name) WITH PARSER ngram;
+
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_kana (kana) WITH PARSER kana;
+
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_mail (mail) WITH PARSER mail;
+
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_departmentName (departmentName) WITH PARSER departmentName;
+
+ALTER TABLE user ADD FULLTEXT INDEX idx_ngram_department_id (department_id) WITH PARSER department_id;
