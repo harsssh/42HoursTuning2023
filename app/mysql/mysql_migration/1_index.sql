d1b5953d8aff447006951e809348aa55411ef283
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-17 09:27:49 +0900

    fix sql

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -1,8 +1,8 @@
 CALL DropIndexIfExists('user', 'idx_mail_password');
 CREATE INDEX idx_mail_password ON user (`mail`, `password`);
 
-CALL DropIndexIfExists('user', 'idx_linked_user_id');
-CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
-
 CALL DropIndexIfExists('user', 'idx_entry_date_kana');
 CREATE INDEX idx_entry_date_kana ON user (`entry_date`, `kana`);
+
+CALL DropIndexIfExists('session', 'idx_linked_user_id');
+CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
