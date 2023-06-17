21eaa6b7db432c9ff91cf6e68b9bd4bb6e26a37b
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-17 09:21:18 +0900

    add index on user

--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -3,3 +3,6 @@ CREATE INDEX idx_mail_password ON user (`mail`, `password`);
 
 CALL DropIndexIfExists('user', 'idx_linked_user_id');
 CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
+
+CALL DropIndexIfExists('user', 'idx_entry_date_kana');
+CREATE INDEX idx_entry_date_kana ON user (`entry_date`, `kana`);
