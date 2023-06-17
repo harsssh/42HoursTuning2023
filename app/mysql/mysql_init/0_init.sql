3d50239097a14fb70d325919046e4891531d423c
harsssh <66548698+harsssh@users.noreply.github.com>
2023-06-17 11:21:51 +0900

    update PK

--- app/mysql/mysql_init/0_init.sql
+++ app/mysql/mysql_init/0_init.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE IF NOT EXISTS app;
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
     `user_id` VARCHAR(36) NOT NULL,
     `employee_id` VARCHAR(50) NOT NULL,
@@ -15,7 +14,6 @@ CREATE TABLE `user` (
     PRIMARY KEY (`user_id`)
 );
 
-DROP TABLE IF EXISTS `session`;
 CREATE TABLE `session` (
     `session_id` VARCHAR(36) NOT NULL,
     `linked_user_id` VARCHAR(36) NOT NULL,
@@ -23,7 +21,6 @@ CREATE TABLE `session` (
     PRIMARY KEY (`session_id`)
 );
 
-DROP TABLE IF EXISTS `department`;
 CREATE TABLE `department` (
     `department_id` VARCHAR(36) NOT NULL,
     `department_name` VARCHAR(50) NOT NULL,
@@ -31,7 +28,6 @@ CREATE TABLE `department` (
     PRIMARY KEY (`department_id`)
 );
 
-DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
     `role_id` VARCHAR(36) NOT NULL,
     `role_name` VARCHAR(50) NOT NULL,
@@ -39,7 +35,6 @@ CREATE TABLE `role` (
     PRIMARY KEY (`role_id`)
 );
 
-DROP TABLE IF EXISTS `department_role_member`;
 CREATE TABLE `department_role_member` (
     `department_id` VARCHAR(36) NOT NULL,
     `role_id` VARCHAR(36) NOT NULL,
@@ -49,14 +44,12 @@ CREATE TABLE `department_role_member` (
     PRIMARY KEY (`department_id`, `role_id`, `user_id`, `entry_date`)
 );
 
-DROP TABLE IF EXISTS `office`;
 CREATE TABLE `office` (
     `office_id` VARCHAR(36) NOT NULL,
     `office_name` VARCHAR(50) NOT NULL,
     PRIMARY KEY (`office_id`)
 );
 
-DROP TABLE IF EXISTS `file`;
 CREATE TABLE `file` (
     `file_id` VARCHAR(36) NOT NULL,
     `file_name` VARCHAR(120) NOT NULL,
@@ -64,21 +57,18 @@ CREATE TABLE `file` (
     PRIMARY KEY (`file_id`)
 );
 
-DROP TABLE IF EXISTS `skill`;
 CREATE TABLE `skill` (
     `skill_id` VARCHAR(36) NOT NULL,
     `skill_name` VARCHAR(50) NOT NULL,
     PRIMARY KEY (`skill_id`)
 );
 
-DROP TABLE IF EXISTS `skill_member`;
 CREATE TABLE `skill_member` (
     `skill_id` VARCHAR(36) NOT NULL,
     `user_id` VARCHAR(36) NOT NULL,
     PRIMARY KEY (`skill_id`, `user_id`)
 );
 
-DROP TABLE IF EXISTS `match_group`;
 CREATE TABLE `match_group` (
     `match_group_id` VARCHAR(36) NOT NULL,
     `match_group_name` VARCHAR(50) NOT NULL,
@@ -89,9 +79,8 @@ CREATE TABLE `match_group` (
     PRIMARY KEY (`match_group_id`)
 );
 
-DROP TABLE IF EXISTS `match_group_member`;
 CREATE TABLE `match_group_member` (
     `match_group_id` VARCHAR(36) NOT NULL,
     `user_id` VARCHAR(36) NOT NULL,
-    PRIMARY KEY (`user_id`, `match_group_id`)
+    PRIMARY KEY (`match_group_id`, `user_id`)
 );
--- app/mysql/mysql_migration/1_index.sql
+++ app/mysql/mysql_migration/1_index.sql
@@ -1,11 +1,14 @@
 CALL DropIndexIfExists('user', 'idx_mail_password');
-CREATE INDEX idx_mail_password ON user (`mail`, `password`);
+CREATE INDEX idx_mail_password ON `user` (`mail`, `password`);
 
 CALL DropIndexIfExists('user', 'idx_entry_date_kana');
-CREATE INDEX idx_entry_date_kana ON user (`entry_date`, `kana`);
+CREATE INDEX idx_entry_date_kana ON `user` (`entry_date`, `kana`);
 
 CALL DropIndexIfExists('session', 'idx_linked_user_id');
-CREATE INDEX idx_linked_user_id ON session (`linked_user_id`);
+CREATE INDEX idx_linked_user_id ON `session` (`linked_user_id`);
+
+ALTER TABLE `match_group_member` DROP PRIMARY KEY;
+ALTER TABLE `match_group_member` ADD PRIMARY KEY (`user_id`, `match_group_id`);
 
 ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_user_name (`user_name`) WITH PARSER ngram;
 ALTER TABLE `user` ADD FULLTEXT INDEX idx_ngram_kana (`kana`) WITH PARSER ngram;
